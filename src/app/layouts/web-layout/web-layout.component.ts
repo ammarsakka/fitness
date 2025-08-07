a349751835480b9b2f45f787e48b87e73d34e0dd
Ammar Sakka <ammar.sakka@agmo.group>
2025-08-07 02:38:40 +0000

    Merged PR 70481: feat: add web layout
    feat: add web layout
    
    Related work items: #76352

---
+++ src/app/layouts/web-layout/web-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-web-layout',
+    standalone: false,
+    templateUrl: './web-layout.component.html',
+    styleUrl: './web-layout.component.scss',
+})
+export class WebLayoutComponent {}
---
+++ src/app/layouts/web-layout/web-layout.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { WebLayoutComponent } from './web-layout.component';
+
+@NgModule({
+    declarations: [WebLayoutComponent],
+    imports: [CommonModule],
+})
+export class WebLayoutModule {}
