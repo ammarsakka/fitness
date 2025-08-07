b07b82fa30febff27bcd8d26c8296b5eb7e5f6df
Ammar Sakka <ammar.sakka@agmo.group>
2025-08-07 02:27:34 +0000

    Merged PR 70474: feat: add auth layout
    feat: add auth layout
    
    Related work items: #76352

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then((m) => m.AuthLayoutModule),
+            },
+        ],
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AuthLayoutComponent } from './auth-layout.component';
+
+@NgModule({
+    declarations: [AuthLayoutComponent],
+    imports: [CommonModule],
+})
+export class AuthLayoutModule {}
