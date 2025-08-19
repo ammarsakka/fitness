a8f9ac17a09043327f695d25eb89e566da6ee67d
aya2-ze <ayazeno@gmail.com>
2025-08-19 14:19:20 +0300

    feat: generate home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { WebLayoutComponent } from './layouts/web-layout/web-layout.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    {
-        path: '',
-        component: AuthLayoutComponent,
-        children: [
-            {
-                path: '',
-                loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then((m) => m.AuthLayoutModule),
-            },
-        ],
-    },
     {
         path: '',
         component: WebLayoutComponent,
--- src/app/layouts/web-layout/web-layout.module.ts
+++ src/app/layouts/web-layout/web-layout.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 import { WebLayoutRoutingModule } from './web-layout-routing.module';
 import { WebLayoutComponent } from './web-layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-    declarations: [WebLayoutComponent],
+    declarations: [WebLayoutComponent, HeaderComponent, FooterComponent],
     imports: [CommonModule, WebLayoutRoutingModule],
 })
 export class WebLayoutModule {}
