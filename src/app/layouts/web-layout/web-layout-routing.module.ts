63306c608c7de07f741ef116bfc4b5811f1e7533
aya2-ze <110290402+aya2-ze@users.noreply.github.com>
2025-08-19 14:30:43 +0300

    feat: generate home component (#1)

--- src/app/layouts/web-layout/web-layout-routing.module.ts
+++ src/app/layouts/web-layout/web-layout-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WEB_ROUTES } from '../../features/web/web-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [...WEB_ROUTES];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
